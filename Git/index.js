7153f2a6ea44733cc6b1c9a8197e29def1c80c52
pushkar <you@example.com>
2024-08-12 09:57:16 +0530

    dkfd

--- Git/index.js
+++ Git/index.js
@@ -1,6 +1,8 @@
-console.log('git version 1');
 
+console.log('git version 1');
+console.log('git version 1');
 //git init
 //git status 
 // git add .add
+// git commit -m "msg"
 // git commit -m "msg"
